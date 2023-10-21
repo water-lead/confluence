6cd94f95f67c3108685d96876e4c593ad32ae799
water-lead <148636323+water-lead@users.noreply.github.com>
2023-10-21 15:48:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -24,5 +24,5 @@ function exportToDOCX() {
 
 // Initially, show the first page and hide the others
 document.getElementById('page1').style.display = 'block';
-document.getElementById('page2').style.display = 'block';
+document.getElementById('page2').style.display = 'none';
 document.getElementById('page3').style.display = 'none';

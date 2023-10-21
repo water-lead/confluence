dcdd9e6959292951ffabacbb5cd9a853e679e260
water-lead <148636323+water-lead@users.noreply.github.com>
2023-10-21 19:26:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-// script.js
+// Define the current page using a variable
+let currentPage = 1;
 
 // Function to move to the next page
-function moveToPage(pageNumber) {
+function showNextPage() {
     // Hide the current page
-    document.getElementById('page' + pageNumber).style.display = 'none';
+    document.getElementById('page' + currentPage).style.display = 'none';
+
+    // Determine the next page
+    currentPage++;
+
+    if (currentPage > 4) {
+        currentPage = 1; // If it exceeds the number of pages, reset to the first page
+    }
 
     // Show the next page
-    pageNumber++;
-    document.getElementById('page' + pageNumber).style.display = 'block';
+    document.getElementById('page' + currentPage).style.display = 'block';
 }
 
 // Function to export results to PDF
@@ -23,19 +30,4 @@ function exportToDOCX() {
 }
 
 // Initially, show the first page and hide the others
-document.getElementById('page1').style.display = 'block';
-document.getElementById('page2').style.display = 'none';
-document.getElementById('page3').style.display = 'none';
-document.getElementById('page4').style.display = 'none';
-
-function showNextPage(currentPage) {
-    // Determine the next page
-    const nextPage = currentPage + 1;
-            // Hide all pages
-            for (let i = 1; i <= 4; i++) {
-   
-            // Show the specified page
-            document.getElementById('page' + i).style.display = 'block';
-}              
-     document.getElementById('page' + i).style.display = 'n';
-            }  
+showNextPage(); // This will show the first page initially

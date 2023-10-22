ce023e584586a80cd6af03487dc92ae2a99b4928
water-lead <148636323+water-lead@users.noreply.github.com>
2023-10-22 02:07:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,25 @@
-// Define the current page using a variable
-let currentPage = 0;
 
-// Function to move to the next page
-function showNextPage() {
-    // Hide the current page
+let currentPage = 1;
+
+function showNextPage(nextPage) {
     document.getElementById('page' + currentPage).style.display = 'none';
-
-    // Determine the next page
-    currentPage++;
-
-    if (currentPage > 4) {
-        currentPage = 0; // If it exceeds the number of pages, reset to the first page
-    }
-
-    // Show the next page
+    currentPage = nextPage;
     document.getElementById('page' + currentPage).style.display = 'block';
 }
 
-// Function to export results to PDF
 function exportToPDF() {
     // Implement PDF export logic here
     alert('Exporting to PDF...');
 }
 
-// Function to export results to DOCX
 function exportToDOCX() {
     // Implement DOCX export logic here
     alert('Exporting to DOCX...');
 }
 
 // Initially, show the first page and hide the others
-showNextPage(); // This will show the first page initially
+document.getElementById('page1').style.display = 'block';
+document.getElementById('page2').style.display = 'none';
+document.getElementById('page3').style.display = 'none';
+document.getElementById('page4').style.display = 'none';
+

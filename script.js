d4694bb8c308ac5bc067fc45ee8cf7ce36357d11
water-lead <148636323+water-lead@users.noreply.github.com>
2023-10-23 21:22:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ function showNextPage(nextPage) {
     document.getElementById('page' + currentPage).style.display = 'block';
 }
 
+function showPreviousPage(previousPage) {
+    document.getElementById('page' - currentPage).style.display = 'none';
+    currentPage = previousPage;
+    document.getElementById('page' - currentPage).style.display = 'block';
+}
+
 function exportToPDF() {
     // Implement PDF export logic here
     alert('Exporting to PDF...');
@@ -22,3 +28,9 @@ document.getElementById('page1').style.display = 'block';
 document.getElementById('page2').style.display = 'none';
 document.getElementById('page3').style.display = 'none';
 document.getElementById('page4').style.display = 'none';
+document.getElementById('page5').style.display = 'none';
+document.getElementById('page6').style.display = 'none';
+document.getElementById('page7').style.display = 'none';
+document.getElementById('page8').style.display = 'none';
+document.getElementById('page9').style.display = 'none';
+document.getElementById('page10').style.display = 'none';

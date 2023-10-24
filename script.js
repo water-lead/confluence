73029df615191e95904d289d71e655a96007a8a5
water-lead <148636323+water-lead@users.noreply.github.com>
2023-10-24 09:19:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_pNw4ZIfdKNWunsCxMuHAotCBVPITa3I",
+  authDomain: "confluence-auth-8d9d6.firebaseapp.com",
+  projectId: "confluence-auth-8d9d6",
+  storageBucket: "confluence-auth-8d9d6.appspot.com",
+  messagingSenderId: "240489843371",
+  appId: "1:240489843371:web:758608b4ab14c536b745ee",
+  measurementId: "G-0F4C8M353B"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+const analytics = getAnalytics(app);
 
 let currentPage = 1;
+
+function authentication(step){
+    if (step === 1) {
+        const email = prompt("Please enter your email for verification:");
+
+        // Check if email is in the invitations collection in Firestore
+        const docRef = doc(db, "invitations", email);
+        getDoc(docRef).then((docSnapshot) => {
+            if (docSnapshot.exists()) {
+                // Email exists in the invitation collection
+                // Redirect or show a registration form here
+                window.location.href = "studio.html";
+            } else {
+                alert("Sorry, you do not have an invitation.");
+            }
+        });
+    }
+}
 
 function showNextPage(nextPage) {
     document.getElementById('page' + currentPage).style.display = 'none';
